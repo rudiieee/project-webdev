0289cd1415877ad774dbbc6cdbc006ba31556916
Rodolfo <rodolfo.olivares@ibm.com>
2021-05-25 21:50:11 -0500

    adfasd

---
+++ Project/rodolfoswebpage.js
@@ -0,0 +1,48 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 9567);
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+    res.render('home.html');
+});
+
+// app.post('/',function(req,res){
+//     var queryParams = [];
+//     var bodyParams = [];
+//     for (var param in req.query){
+//         queryParams.push({'name':param,'value':req.query[param]})
+//     }
+//     for (var param in req.body){
+//         bodyParams.push({'name':param,'value':req.body[param]})
+//     }
+//     var data = {};
+//     data.urlList = queryParams;
+//     data.bodyList = bodyParams;
+//     res.render('post-page', data);
+// });
+
+// app.use(function(req,res){
+//     res.status(404);
+//     res.render('404');
+// });
+
+// app.use(function(err, req, res, next){
+//     console.error(err.stack);
+//     res.type('plain/text');
+//     res.status(500);
+//     res.render('500');
+// });
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://flip2.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
