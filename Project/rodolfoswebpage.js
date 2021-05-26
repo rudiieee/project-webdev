cdb400eff519fb08556d4ac08f2f635b462a186b
Rodolfo <rodolfo.olivares@ibm.com>
2021-05-25 21:56:48 -0500

    afdaf

--- Project/rodolfoswebpage.js
+++ Project/rodolfoswebpage.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/',function(req,res){
-    res.render('home.html');
+    res.render('home');
 });
 
 // app.post('/',function(req,res){
